9eecf4c285f135bde85ab34e3c6710c7e11b5009
amiika <amiika@gmail.com>
2018-09-04 18:41:55 +0300

    Create examples.rb
    Added example file for quick testing

---
+++ examples.rb
@@ -0,0 +1,17 @@
+# frere jacques
+zplay("|:q1231:|:q34h5:|@:e5654q31:|:q1-5+h1:@|")
+
+# ode to joy
+zplay("|:q3345|5432|1123|;q32h2;q21h1:|q2231|2e34q31|2e34q32|q12h-5|+q3345|5432|1123|21h1|")
+
+# twinkle twinkle
+zplay("q115566h5q443322h1 *|: q554433h2 :|*")
+
+# row row
+zplay("|:q.1.1|q1e2q.3|3e2q3e4|h.5|e888555333111|q5e4q3e2|h.1:|")
+
+# Ski or die
+zsynth("h3q323 q ~0.1 3666 h5.3 q ~0.25 3666 53 q ~0.2 3232222",{synth:"chipbass"})
+
+# Some drum sounds
+zplay("~1 1-7 ~0.5 +1--7 ~0.25 ++1---7")
