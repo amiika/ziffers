5baa15c7b5e3cde2a2bdd4617543a5dc1b3b1735
amiika <amiika@gmail.com>
2018-09-19 21:32:30 +0300

    Update ziffers.rb
    Added zsample that can play numbered notation with pitch modulated samples

--- ziffers.rb
+++ ziffers.rb
@@ -20,6 +20,27 @@ def synthDefaults
   }
 end
 
+def defaultSampleOpts
+  defaults = {
+    :sample => :ambi_piano,
+    :rate => 1,
+    :key => :c,
+    :scale => :major,
+    :pan => 0,
+    :attack => 0.0,
+    :decay => 0.0,
+    :release => 0.0,
+    :sustain => sample_duration(:ambi_piano),
+    :sleep => 0.25,
+    :pitch => 0.0,
+    :amp => 1.0,
+    :amp_slide => 0,
+    :amp_step => 0.5,
+    :note_slide => 0.5,
+    :control => nil
+  }
+end
+
 def defaultOpts
   defaults = {
     :key => :c,
@@ -40,8 +61,8 @@ def defaultOpts
   }
 end
 
-def getTypes
-  type = {
+def getControlChars
+  controlChars = {
     'V': :amp, # Volume
     'E': :env_curve,
     'A': :attack,
@@ -54,7 +75,7 @@ def getTypes
     'K': :key,
     '$': :scale,
     '~': :note_slide,
-    '^': :synth, # Instrument
+    '^': :synth,
     'C': :chord,
   }
 end
@@ -64,271 +85,314 @@ def getScaleDegrees(zkey,zscale)
 end
 
 def zparse(n,opts=nil)
-  notes, noteBuffer, controlBuffer, rnd, chs = Array.new(5){ [] }
-  loop, rndRange, rndChoose, escape, dc, ds = false, false, false,false,false,false
+  notes, noteBuffer, controlBuffer, rnd = Array.new(4){ [] }
+  loop, escape, dc, ds = false, false, false,false
   stringFloat = ""
   noteLength, dotLength = 0.25, 1.0
-  addition, sfaddition, dot, loopCount = 0, 0, 0, 0
-  slideNext = false
+  sfaddition, dot, loopCount = 0, 0, 0, 0
+  skip, slideNext = false, false
   escapeType = nil
   durs = defaultDurs
   defaults = defaultOpts
   if opts!=nil then
     defaults = defaults.merge(opts)
   end
-  type = getTypes
+  controlChars = getControlChars
   durs.default = 0.25
-  ziff = defaults.clone
+  # Clone defaults to current node
+  ziff, controlZiff = defaults.clone
+  # Merge synth options to defaults
   defaults = synthDefaults.merge(defaults)
-  zkey = ziff.fetch(:key)
-  zscale = ziff.fetch(:scale)
-  scaleDegrees = getScaleDegrees(zkey,zscale)
+  
   # Loop chars
   chars = n.chars
   chars.to_enum.each_with_index do |c, index|
     next_c = chars[index+1]
     dgr = nil
-    case c
-    when '!' then
-      addition = 0
-      slideNext = false
-      ziff = ziff.merge(defaults)
-    when /^[A-Z]+$/ then
-      if !escape && type.key?(c.to_sym) then
-        escape = true
-        escapeType = type[c.to_sym]
-      else
-        stringFloat+=c
-      end
-    when ' '
-      if escape then
-        #print escapeType.to_s+":"+stringFloat.to_s
-        if stringFloat == nil || stringFloat.length==0 then
-          ziff[escapeType] = defaults.fetch(escapeType)
-        elsif escapeType == :scale then
-          ziff[escapeType] = searchList(scale_names, stringFloat)
-          # If scale changes get new ring of degrees
-          zscale = ziff.fetch(:scale)
-          scaleDegrees = getScaleDegrees(zkey, zscale)
-        elsif escapeType == :synth then
-          ziff[escapeType] = searchList(synth_names, stringFloat)
-        elsif escapeType == :key then
-          ziff[escapeType] = stringFloat.to_s
-        elsif escapeType == :chord then
-          zchord = chord_degree(stringFloat.to_i,zkey,zscale,3) # Add more options?
-          ziff[escapeType] = zchord
-        elsif escapeType == :sleep then
-          noteLength = stringFloat.to_f
-          ziff[escapeType] = stringFloat.to_f
-        else
-          ziff[escapeType] = stringFloat.to_f
-        end
-        stringFloat = ""
-        escape = false
-      end
-    when '.' then
-      if escape then
-        stringFloat = stringFloat+c
-      else
-        dot+=1
-        dotLength = (2.0-(1.0/(2**dot)))
-      end
-    when /^[a-z]+$/ then
-      # Set note length
-      if escape then
-        stringFloat+=c
-      else
-        noteLength = durs[c.to_sym]
-      end
-    when /^[0-9]+$/ then
-      # Notes inside () or []
-      if rndRange || rndChoose then
-        if rndRange then
-          if rnd.length > 2 then raise 'Too many parameters' end
-          rnd.push(c.to_i)
-        elsif rndChoose then
-          chs.push(c.to_i)
-        end
-      elsif escape then
-        stringFloat = stringFloat+c
-      else
-        # Plain notes 1234 etc.
-        dgr = c.to_i
-      end
-    when '?' then
-      dgr = rrand_i(1,scaleDegrees.length)
-    when '#' then
-      sfaddition += 1
-    when '&' then
-      sfaddition -= 1
-    when '+' then
-      addition += 12
-    when '-' then
-      if escape then
-        stringFloat = stringFloat+c
-      else
-        addition += -12
-      end
-    when '<' then
-      ziff[:amp] = ziff.fetch(:amp)+ziff.fetch(:amp_step)
-    when '>' then
-      ziff[:amp] = ziff.fetch(:amp)-ziff.fetch(:amp_step)
-    when '%' then
-      ziff[:pan] = [1,-1,0].choose
-    when '~' then
-      escape = true
-      escapeType = type[c.to_sym]
-    when '^' then
-      escape = true
-      escapeType = type[c.to_sym]
-    when '$' then
-      if next_c==' ' then
-        randomScale = scale_names.choose
-        ziff[:scale] = randomScale
-        scaleDegrees = getScaleDegrees(zkey, randomScale)
-      else
-        escapeType = type[c.to_sym]
-        escape = true
-      end
-    when ':' then
-      if noteBuffer.length > 0 then
-        # Normal loop must be ending, add buffer to note list
-        if loopCount<2 then
-          notes = notes.concat(noteBuffer)
-        end
-        loopCount = 0
-        noteBuffer = []
-        loop = false
-      else
-        # Loop is starting
-        loop = true
-      end
-    when ";" then
-      if noteBuffer.length > 0 then
-        loopCount+=1
-        if loopCount>1 then
-          notes = notes.concat(noteBuffer)
-        end
-      else
-        raise "Use of : is mandatory before ;"
-      end
-    when '(' then
-      rndRange = true
-    when ')' then
-      rndRange = false
-      dgr = rrand_i(rnd[0],rnd[1])
-      rnd = []
-    when '[' then
-      rndChoose = true
-    when ']' then
-      rndChoose = false
-      dgr = chs.choose
-      chs = []
-    when '@' then
-      # Recursive call from @ to @
-      if ds then
-        again = zparse(n[/\@(.*?)@/,1], ziff.clone)
-        notes = notes.concat(again)
-      else
-        ds = !ds
-      end
-    when '*' then
-      # Recursive call from beginning to first !
-      if dc then
-        again = zparse(n.split('*')[0], ziff.clone)
-        notes = notes.concat(again)
-      else
-        dc = !dc
-      end
-    else
-      # Spaces, commas and all other nonsense
-    end
     
-    # If any degree was parsed, parse note and add to hasharray
-    if dgr!=nil then
-      #print "Parsed degree: "+dgr.to_s
-      note = nil
-      if dgr==0 then
-        note = :r
-      else
-        zkey = ziff.fetch(:key)
-        zscale = ziff.fetch(:scale)
-        # Transpose if needed
-        if dgr>scaleDegrees.length then
-          note = degree(scaleDegrees[dgr],zkey,zscale)
-          note+=12
+    if skip then
+      skip = false # Skip next char and continue
+    else
+      case c
+      when '!' then
+        # Reset node options to defaults
+        addition = 0
+        slideNext = false
+        ziff = ziff.merge(defaults)
+      when /^[A-Z]+$/ then
+        if !escape && controlChars.key?(c.to_sym) then
+          # If char Following chars are readed to stringFloat until next ' '
+          escape = true
+          escapeType = controlChars[c.to_sym]
+          print "escape: "+escapeType.to_s
         else
-          note = degree(dgr,zkey,zscale)
+          stringFloat+=c
         end
-        # Add sharps and flats
-        note = note + sfaddition
-        sfaddition = 0
-        # Add +- additions
-        note = note + addition
-      end
-      
-      if slideNext then
-        
-        #Slide starts
-        controlZiff = ziff.clone
-        controlZiff[:note] = note
-        controlZiff[:sleep] = controlZiff[:sleep]*dotLength
-        # Remove nonmodulatable params
-        controlZiff.delete(:attack)
-        controlZiff.delete(:release)
-        controlZiff.delete(:sustain)
-        controlZiff.delete(:decay)
-        
-        controlBuffer.push(controlZiff)
-        
-        if next_c == nil || next_c == ' ' then
-          #print "Slide ends: "+controlBuffer.length.to_s
-          slideNext = false
-          ziff[:control] = controlBuffer
-          ziff[:release] = ziff[:sleep] * (ziff[:control].length+1)
-          controlBuffer = []
+      when ' '
+        # Parse current string or float
+        if escape then
+          #print escapeType.to_s+":"+stringFloat.to_s
+          if stringFloat == nil || stringFloat.length==0 then
+            ziff[escapeType] = defaults.fetch(escapeType)
+          elsif escapeType == :scale then
+            ziff[:scale] = searchList(scale_names, stringFloat)
+          elsif escapeType == :synth then
+            ziff[:synth] = searchList(synth_names, stringFloat)
+          elsif escapeType == :key then
+            ziff[:key] = stringFloat.to_s
+          elsif escapeType == :chord then
+            ziff[:chord] = chord_degree(stringFloat.to_i,ziff[:key],ziff[:scale],3) # Add more options?
+          elsif escapeType == :sleep then
+            noteLength = stringFloat.to_f
+            ziff[:sleep] = noteLength
+          else
+            print stringFloat
+            ziff[escapeType] = stringFloat.to_f
+            print ziff[escapeType]
+          end
+          stringFloat = ""
+          escape = false
         end
-        
+      when '.' then
+        if escape then
+          stringFloat = stringFloat+c
+        else
+          dot+=1
+          # https://en.wikipedia.org/wiki/Dotted_note
+          dotLength = (2.0-(1.0/(2**dot)))
+        end
+      when /^[a-z]+$/ then
+        # Set note length
+        if escape then
+          stringFloat+=c
+        else
+          noteLength = durs[c.to_sym]
+        end
+      when /^[0-9]+$/ then
+        # Notes inside () or []
+        if escape then
+          if escapeType == 'rrandom' then
+            if rnd.length > 2 then raise 'Too many parameters' end
+            rnd.push(c.to_i)
+          elsif escapeType == 'choose' then
+            rnd.push(c.to_i)
+          elsif escape then
+            stringFloat = stringFloat+c
+          end
+        else
+          # Plain notes 1234 etc.
+          dgr = c.to_i
+        end
+      when '?' then
+        if escape then
+          if escapeType == :pan then
+            ziff[:pan] = [1,-1,0].choose
+            escapeType = nil
+            escape = false
+          else
+            stringFloat = stringFloat+rrand_i(0,7).to_s
+          end
+        else
+          dgr = rrand_i(1,getScaleDegrees(ziff[:key],ziff[:scale]).length)
+        end
+      when '#' then
+        sfaddition += 1
+      when '&' then
+        sfaddition -= 1
+      when '+' then
+        ziff[:pitch] += 12
+      when '-' then
+        if escape then
+          stringFloat = stringFloat+c
+        else
+          ziff[:pitch] -= 12
+        end
+      when '<' then
+        ziff[:amp] = ziff.fetch(:amp)+ziff.fetch(:amp_step)
+      when '>' then
+        ziff[:amp] = ziff.fetch(:amp)-ziff.fetch(:amp_step)
+      when '%' then
+        # Todo
+      when '~' then
+        escape = true
+        escapeType = controlChars[c.to_sym]
+      when '^' then
+        escape = true
+        escapeType = controlChars[c.to_sym]
+      when '$' then
+        if next_c==' ' then
+          randomScale = scale_names.choose
+          ziff[:scale] = randomScale
+        else
+          escapeType = controlChars[c.to_sym]
+          escape = true
+        end
+      when ':' then
+        if noteBuffer.length > 0 then
+          # Loop must be ending, add buffer notes to list
+          if loopCount<1 then
+            # Skip numbered repeat in ;-loop sections
+            if (Integer(next_c) rescue false) then
+              (next_c.to_i-1).times do
+                notes = notes.concat(noteBuffer)
+              end
+              skip = true # Skip next char
+            else
+              notes = notes.concat(noteBuffer)
+            end
+          end
+          loopCount = 0
+          noteBuffer = []
+          loop = false
+        else
+          # Loop is starting
+          loop = true
+        end
+      when ";" then
+        if noteBuffer.length > 0 then
+          loopCount+=1
+          if loopCount>1 then
+            notes = notes.concat(noteBuffer)
+          end
+        else
+          raise "Use of : is mandatory before ;"
+        end
+      when '(' then
+        escape = true
+        escapeType = 'rrandom'
+      when ')' then
+        escape = false
+        escapeType = nil
+        dgr = rrand_i(rnd[0],rnd[1])
+        rnd = []
+      when '[' then
+        escape = true
+        escapeType = 'choose'
+      when ']' then
+        escape = false
+        escapeType = nil
+        dgr = rnd.choose
+        rnd = []
+      when '@' then
+        # Recursive call from @ to @
+        if ds then
+          again = zparse(n[/\@(.*?)@/,1], ziff.clone)
+          notes = notes.concat(again)
+        else
+          ds = !ds
+        end
+      when '*' then
+        # Recursive call from beginning to first !
+        if dc then
+          again = zparse(n.split('*')[0], ziff.clone)
+          notes = notes.concat(again)
+        else
+          dc = !dc
+        end
       else
+        # Spaces, commas and all other nonsense
+      end
+      
+      # If any degree was parsed, parse note and add to hasharray
+      if dgr!=nil then
+        #print "Parsed degree: "+dgr.to_s
         
-        if escapeType==:note_slide then
-          #print "Start slide"
-          slideNext=true
-          escapeType=nil
+        note = getNoteFromDgr(dgr, ziff[:key], ziff[:scale])
+        
+        if slideNext then
+          
+          controlZiff[:degree] = dgr
+          controlZiff[:note] = note
+          controlZiff[:sleep] = noteLength*dotLength
+          controlZiff[:pitch] = controlZiff[:pitch]+sfaddition
+          
+          controlBuffer.push(controlZiff)
+          controlZiff = controlZiff.clone
+          
+          if sfaddition!=0 then
+            controlZiff[:pitch] = controlZiff[:pitch]-sfaddition
+            sfaddition=0
+          end
+          
+          if next_c == nil || next_c == ' ' then
+            #print "Slide ends: "+controlBuffer.length.to_s
+            slideNext = false
+            ziff[:control] = controlBuffer
+            ziff[:release] = ziff[:sleep] * (ziff[:control].length+1)
+            ziff = ziff.clone
+            ziff[:control] = nil
+            controlBuffer = []
+          end
+          
+        else
+          
+          if escapeType==:note_slide then
+            #print "Start slide"
+            slideNext=true
+            escapeType=nil
+            # Remove nonmodulatable params
+            controlZiff = ziff.clone
+            controlZiff.delete(:attack)
+            controlZiff.delete(:release)
+            controlZiff.delete(:sustain)
+            controlZiff.delete(:decay)
+          end
+          
+          ziff[:degree] = dgr
+          ziff[:note] = note
+          ziff[:sleep] = noteLength*dotLength
+          ziff[:release] = defaults[:release]*(noteLength*2)
+          ziff[:pitch] = ziff[:pitch]+sfaddition
+          
+          # Add note to buffer if looping with :
+          if loop && loopCount<1 then
+            noteBuffer.push(ziff)
+          end
+          
+          notes.push(ziff)
+          
+          if !slideNext then
+            ziff = ziff.clone
+            if sfaddition!=0 then
+              ziff[:pitch] = ziff[:pitch]-sfaddition
+              sfaddition=0
+            end
+          end
+          
         end
         
-        #print "Create new object"
-        ziff = ziff.clone
-        ziff[:control] = nil
-        ziff[:note] = note
-        ziff[:sleep] = noteLength*dotLength
-        ziff[:release] = defaults[:release]*(noteLength*2)
+        dot = 0
+        dotLength = 1.0
         
-        # Add note to buffer if looping with :
+      elsif ziff[:chord]!=nil
+        chordZiff = ziff.clone
+        chordZiff[:control] = nil
+        chordZiff[:sleep] = defaults[:chordSleep]
+        chordZiff[:release] = defaults[:chordRelease]
+        notes.push(chordZiff)
         if loop && loopCount<1 then
-          noteBuffer.push(ziff.clone)
+          noteBuffer.push(chordZiff)
         end
-        
-        notes.push(ziff)
-        
+        ziff.delete(:chord)
       end
-      
-      dot = 0
-      dotLength = 1.0
-      
-    elsif ziff[:chord]!=nil
-      chordZiff = ziff.clone
-      chordZiff[:sleep] = defaults[:chordSleep]
-      chordZiff[:release] = defaults[:chordRelease]
-      notes.push(chordZiff)
-      if loop && loopCount<1 then
-        noteBuffer.push(chordZiff)
-      end
-      ziff.delete(:chord)
+      # Continues loop
     end
-    # Continues loop
   end
   notes
+end
+
+def getNoteFromDgr(dgr, zkey, zscale)
+  if dgr==0 then
+    return :r
+  else
+    scaleDegrees = getScaleDegrees(zkey,zscale)
+    if dgr>scaleDegrees.length then
+      # Transposes up if not enough notes in scale
+      return degree(scaleDegrees[dgr],zkey,zscale)+12
+    else
+      return degree(dgr,zkey,zscale)
+    end
+  end
 end
 
 def searchList(arr,query)
@@ -388,7 +452,7 @@ def zplay(melody,opts={})
         sleep cziff[:sleep]
       end
     else
-      sleep ziff[:sleep] if melody.length>1
+      sleep ziff[:sleep]
     end
   end
 end
@@ -400,8 +464,8 @@ def zbin(melody,opts={hz: 4, right: true})
     midiHz = midi_to_hz(ziff[:note])
     diffNote = hz_to_midi(midiHz+opts[:hz])
     print diffNote
-    c = play ziff[:note], amp: ziff[:amp], pan: 1, attack: ziff[:attack], release: ziff[:release], sustain: ziff[:sustain], decay: ziff[:decay], pitch: ziff[:pitch], note_slide: ziff[:note_slide] if ziff[:note]!=nil
-    d = play diffNote, amp: ziff[:amp], pan: -1, attack: ziff[:attack], release: ziff[:release], sustain: ziff[:sustain], decay: ziff[:decay], pitch: ziff[:pitch], note_slide: ziff[:note_slide] if ziff[:note]!=nil
+    c = play ziff[:note], amp: ziff[:amp], pan: ziff[:pan] == 1 ? -1 : 1, attack: ziff[:attack], release: ziff[:release], sustain: ziff[:sustain], decay: ziff[:decay], pitch: ziff[:pitch], note_slide: ziff[:note_slide] if ziff[:note]!=nil
+    d = play diffNote, amp: ziff[:amp], pan: ziff[:pan] == 1 ? 1 : -1, attack: ziff[:attack], release: ziff[:release], sustain: ziff[:sustain], decay: ziff[:decay], pitch: ziff[:pitch], note_slide: ziff[:note_slide] if ziff[:note]!=nil
     
     if ziff[:control] != nil then
       sleep ziff[:sleep]/3
@@ -460,3 +524,26 @@ def mergeOpts(ziff, opts)
   ziff.merge(opts) {|_,a,b| (a.is_a? Numeric) ? a * b : b }
 end
 
+def zsample(melody,opts=defaultSampleOpts)
+  if melody.is_a? String then
+    melody = zparse(melody,opts)
+    opts = {}
+  end
+  melody.each do |ziff|
+    ziff = mergeOpts(ziff, opts)
+    mscale = scale 1, ziff[:scale]
+    zpitch = mscale[ziff[:degree]-1]+ziff[:pitch]-0.9999
+    c = sample ziff[:sample], rate: ziff[:rate], pitch: zpitch, amp: ziff[:amp], attack: ziff[:attack], decay: ziff[:decay], sustain: ziff[:sustain], release: ziff[:release], pitch_slide: 0.5 if ziff[:degree]!=0
+    if ziff[:control] != nil then
+      sleep ziff[:sleep]/3
+        ziff[:control].each do |cziff|
+        czpitch = mscale[cziff[:degree]-1]+cziff[:pitch]-1
+        control c, pitch: czpitch, amp: ziff[:amp] if ziff[:degree]!=0
+        sleep cziff[:sleep]
+      end
+    else
+      sleep ziff[:sleep] if melody.length>1
+    end
+
+    end
+end
