0ea4f49d30a29afe6ebe06aa3cdc4a77e41dfecc
amiika <amiika@gmail.com>
2018-09-24 21:31:18 +0300

    Update examples.rb
    Changed chord notation

--- examples.rb
+++ examples.rb
@@ -1,19 +1,4 @@
 use_synth :piano
-
-def testsingledegrees
-  (scale :gong).reflect.each do |k|
-    [1,6].each do |d|
-      zplay d, key: 40+k, scale: :blues_minor
-      sleep 0.1
-    end
-  end
-end
-
-def testarraydegrees
-  zplay [4,4,3,3,2,2,3,3,4,4,3,3,2,2,3,3], key: :c, scale: :chromatic, sleep: 0.12
-  zplay (scale :gong).reflect.to_a,  key: 60, scale: :blues_minor
-end
-
 def testzplay
   # frere jacques
   zplay("|:q1231:|:q34h5:|@:e5654q31:|:q1-5+h1:@|")
@@ -29,24 +14,24 @@ def testzplay
   zplay("|:1234:3|616|:4321:3|")
 end
 
-def testzdrums
-  # Some drum sounds
-  zdrums("1 2 3 4")
-end
-
-def testzsynth
-  # Ski or die
-  zsynth("h3q323 q ~0.1 3666 h5.3 q ~0.25 3666 53 q ~0.2 3232222",{synth:"chipbass"})
-  # Chord synth test
-  zsynth("|:C4 123 C3 234 C2 432 C1 123:|",{key: "e", scale: "mixolydian"})
-end
-
-def testzbin
-  # Binaural twinkle
-  m = zparse("q115566h5q443322h1 *|: q554433h2 :|*")
-  with_synth :sine do
-    zbin(m,{hz:10})
+def testslide
+  with_synth :chiplead do
+    zplay("h3q323 ..q ~0.15 36 h5.3 ..q ~0.25 36 53 q ~0.25 3232222")
   end
+end
+
+def testsingledegrees
+  (scale :gong).reflect.each do |k|
+    [1,3,6].each do |d|
+      zplay d, key: 40+k, scale: :blues_minor
+      sleep 0.1
+    end
+  end
+end
+
+def testarraydegrees
+  zplay [4,4,3,3,2,2,3,3,4,4,3,3,2,2,3,3], key: :c, scale: :chromatic, sleep: 0.12
+  zplay (scale :gong).reflect.to_a,  key: 60, scale: :blues_minor
 end
 
 def testrandom
@@ -55,19 +40,39 @@ def testrandom
   end
 end
 
+def testzdrums
+  # Some drum sounds
+  zdrums("1 2 3 4")
+end
+
+def testchords
+  # Chord synth test
+  zplay("i ii iii iv v vi vii",{key: "e", scale: "major", chordSleep: 0.25, chordSynth: :piano})
+  zplay("|:iv 123 iii 234 ii 432 i 123:|",{key: "e", scale: "mixolydian"})
+end
+
+def testbinaural
+  with_synth :beep do zplay("q12345678") end
+  with_synth :beep do zplay("q12345678",{hz:10}) end
+  zsample("q12345678",{sustain: 0.25, sample: :ambi_glass_rub})
+  zsample("q12345678",{hz:10, sustain: 0.25, sample: :ambi_glass_rub})
+  zsample("q12345678",{sustain: 0.25, sample: :ambi_glass_rub},true)
+  zsample("q12345678",{hz:10, sustain: 0.25, sample: :ambi_glass_rub},true)
+end
+
 def testzsample
-  n = zparse("|:q1231:|:q34h5:|@:e5654q31:|:q1-5+h1:@|", {sample:  :ambi_drone, sustain: 0.25, key: "c1"}, defaultSampleOpts)
-  zsample(n, {}, true)
   zsample("|:q1231:|:q34h5:|@:e5654q31:|:q1-5+h1:@|", {sample:  :ambi_drone, key: "c1", sustain: 0.25})
+  zsample("|:q1231:|:q34h5:|@:e5654q31:|:q1-5+h1:@|", {sample:  :ambi_drone, key: "c1", sustain: 0.25},true)
   zsample("h3q323 q ~0.1 3666 h5.3 q ~0.25 3666 53 q ~0.2 3232222",{sample: :ambi_piano, sustain: 0.25, key: "c"})
   zsample("q115566h5q443322h1 *|: q554433h2 :|*", sample: :ambi_glass_rub, rate: 2.1, amp: 0.2)
 end
 
+testzplay
+testslide
 testsingledegrees
 testarraydegrees
-testzplay
-testzsynth
+testchords
 testzdrums
-testzbin
+testbinaural
 testrandom
 testzsample
